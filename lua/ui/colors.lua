87dab72779fdc85f1fbee8c47c47ce713f742975
Anshuman <amedhi@connect.ust.hk>
2024-05-19 00:00:59 +0800

    maintained fork

--- lua/ui/colors.lua
+++ lua/ui/colors.lua
@@ -1,18 +1,20 @@
 -- TODO: set lualine theme
 return {
   {
-    "norcalli/nvim-colorizer.lua",
-    event = "BufWinEnter",
+    "nvchad/nvim-colorizer.lua",
+    event = { "BufWinEnter" },
     config = function()
-      require("colorizer").setup({ "*" }, {
+      require("colorizer").setup {
         RGB = true, -- #RGB hex codes
+        names = false,
         RRGGBB = true, -- #RRGGBB hex codes
         RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
         rgb_fn = true, -- CSS rgb() and rgba() functions
         hsl_fn = true, -- CSS hsl() and hsla() functions
         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      })
+      }
       -- names    = true;         -- "Name" codes like Blue
 
       vim.cmd "ColorizerReloadAllBuffers"
@@ -50,7 +52,7 @@ return {
   { --tzachar/local-highlight.nvim
     "tzachar/local-highlight.nvim",
     opts = {
-      disable_file_types = { "tex" },
+      disable_file_types = { "tex", "lua" },
       -- hlgroup = "Underlined",
       -- hlgroup = "Search",
     },
